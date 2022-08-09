d6a40e55d2eef296b7767827ee0a65c072bad48e
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-08-09 22:28:33 +0400

    task3

--- tast2/Program.cs
+++ tast2/Program.cs
@@ -6,4 +6,9 @@
 
 
 Console.WriteLine("Введите целое число: ");
+int a = Convert.ToInt32(System.Console.ReadLine());
+Console.Write("Введите еще одно целое число: ");
+int b = Convert.ToInt32(System.Console.ReadLine());
 
+if(b*b == a) Console.WriteLine($"число {a} является квадратом числа {b}");
+else Console.WriteLine($"число {a} НЕ является квадратом числа {b}");
