d9f019c0612dc1455728fb238417ed1b33e1dab4
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-08-09 22:11:06 +0400

    Задача1

--- tast2/Program.cs
+++ tast2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая на вход принимает 2 числа и проверяет,является лт первое число квадратом второго.
+// а = 25, b  = 5 -> да
+// а = 2, b  = 10 -> нет
+// а = 9, b  = -3 -> да
+// а = -3, b  = 9 -> нет
+
+
+Console.WriteLine("Введите целое число: ");
+
